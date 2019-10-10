49fcdc055570d69398bf347b74303967c4e5d9ae
ruant <1284957262@qq.com>
2019-10-10 10:53:43 +0800

    Initial commit

---
+++ rim-route/src/main/java/cn/rt/route/RimRouteApplication.java
@@ -0,0 +1,13 @@
+package cn.rt.route;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RimRouteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RimRouteApplication.class, args);
+    }
+
+}
